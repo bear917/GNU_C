126b6caef42fc0bd8e4eb2b6362c17a4e82df01d
bear917 <bear917@users.noreply.github.com>
2015-08-23 17:01:07 +0800

    add type

---
+++ bicycles/bicycles1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int number_of_bicycles;
+    int number_of_wheels;
+
+    number_of_bicycles = 6;
+    number_of_wheels = number_of_bicycles * 2;
+
+    printf("I have %d bicycles\n", number_of_bicycles);
+    printf("So I have %d wheels\n", number_of_wheels);
+
+    return 0;
+}
---
+++ type/displaying_variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    short first_number = -5;
+    long second_number, third_number;
+
+    second_number = 20000 + 10000;
+
+    printf("the value of first_number is %hd\n", first_number);
+    printf("the value of second_number is %ld\n", second_number);
+    printf("the value of third_number is %ld\n", third_number);
+
+    return 0;
+}
