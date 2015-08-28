8319689b91a34104f8dab791465045e3797a541e
bear917 <bear917@gmail.com>
2015-08-28 15:09:21 +0800

    modified number square

--- array/number_square.c
+++ array/number_square.c
@@ -13,7 +13,13 @@ int main() {
 
     for (row = 0; row < num_row; row++)
         for (col = 0; col < num_col; col++) {
-                printf("[%d][[%d] = %4d\n", row, col, matrix[row][col]); 
+				if (col == (num_col-1)) {
+					// printf("[%d][[%d] = %4d\n", row, col, matrix[row][col]);
+					printf("%4d\n", matrix[row][col]);
+				} else {
+					// printf("[%d][[%d] = %4d", row, col, matrix[row][col]); 				
+					printf("%4d", matrix[row][col]); 				
+				}
             }
 
     return 0;
