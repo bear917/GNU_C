40fc5b43ff7945516c89dde852a14d8b04e814dd
bear917 <bear917@users.noreply.github.com>
2015-11-19 10:10:59 +0800

    add conut up/down

--- my_deck/cards.c
+++ my_deck/cards.c
@@ -23,7 +23,16 @@ int main() {
     } else {
         val = atoi(card_name);
     }
+
     printf("The card value is: %i\n", val);
+
+    /* check if the value is 3 to 6 */
+    if (val>=3 && val<=6) 
+        puts("Count has gone up");
+    /* Otherwise check if the card was 10, J, Q, or K */
+    else if (val>=10 && val<=11) 
+        puts("Conut has gone down");
+
 
     return 0;
 }
